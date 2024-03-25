c7a4bc5cf6d45fd681a07f268bf6871faeaec183
JarvisChan666 <744765386@qq.com>
2024-03-25 15:11:01 +0800

    first commit

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "mtc-rg" {
+  name     = "mtc-resources"
+  location = "East Us"
+  tags = {
+    environment = "dev"
+  }
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "mtc-vn" {
+  name                = "mtc-network"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  address_space       = ["10.0.0.0/16"]
+  tags = {
+    environment = "dev"
+  }
+}
